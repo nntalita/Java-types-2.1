5052502444ca231bd8ed3db41a6f4996de6e51f4
Natalya Pletneva <nntalita@gmail.com>
2021-09-11 17:51:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticketPrice = 1545;
+        int amountRublesForBonus = 20;
+        int bonusMile = ticketPrice / amountRublesForBonus;
+        System.out.println(bonusMile);
+    }
+}
